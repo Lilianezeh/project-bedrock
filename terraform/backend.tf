e764113f660dd2b7205cf0043cafdfb61c9242ff
Ezeh Lilian <lilianezichi@gmail.com>
2026-06-20 12:06:47 +0100

    Redeploy to new AWS account 304896961232 after suspension; restore HTTPS with new ACM cert and Route53 zone

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "bedrock-tfstate-alt-soe-025-5701"
+    bucket  = "project-bedrock-tfstate-304896961232"
     key     = "project-bedrock/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
